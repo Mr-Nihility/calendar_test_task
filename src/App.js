4efe0e3b9db903eba3352bf1c9cb0b2480cf6315
Volodymyr Romanenko <98472440+Mr-Nihility@users.noreply.github.com>
2022-09-05 18:10:32 +0300

    init commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Calendar from "./Calendar/Calendar";
+import Header from "./Header/Header";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Calendar></Calendar>
+    </>
+  );
+}
+
+export default App;
